4dddf85518701f2d835576f73f79bb5c6d64c5e8
Marianne-Orts <ma.orts.ma@gmail.com>
2021-01-30 18:26:57 +0100

    Add change palette functionality

---
+++ src/js/02 palette.js
@@ -0,0 +1,15 @@
+"use strict";
+const paletteElements = document.querySelectorAll(".js-palette");
+const cardElement = document.querySelector(".js-card");
+
+function handlePalette() {
+  cardElement.classList.remove("palette01", "palette02", "palette03");
+  const checkedPalette = document.querySelector(".js-palette:checked");
+  const checkedPaletteValue = checkedPalette.value;
+  cardElement.classList.add("palette" + checkedPaletteValue);
+}
+for (const paletteElement of paletteElements) {
+  paletteElement.addEventListener("change", handlePalette);
+}
+
+handlePalette();
