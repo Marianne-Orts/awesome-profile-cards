c63e1bf83f024420fb3f8fefe23ec042a8685d3d
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-02-01 12:48:57 +0100

    improve collapsable functionality

--- src/js/01 collapsable.js
+++ src/js/01 collapsable.js
@@ -1,16 +1,29 @@
-"use strict";
+'use strict';
 //solo afecta a diseña pq al efectar a legend solo afecta al primer hijo.
-const legendDesignElement = document.querySelector(".legend__design");
-const legendRellenaElement = document.querySelector(".legend__rellena");
-const legendComparteElement = document.querySelector(".legend__comparte");
-const collapsableElement = document.querySelector(".collapsable");
+// const legendDesignElement = document.querySelector(".legend__design");
+// const legendRellenaElement = document.querySelector(".legend__rellena");
+// const legendComparteElement = document.querySelector(".legend__comparte");
+// const collapsableElement = document.querySelector(".collapsable");
 
-function handleCollapsable() {
+// function handleCollapsable() {
 
-  collapsableElement.classList.toggle("collapsable--on");
+//   collapsableElement.classList.toggle("collapsable--on");
 
+// }
+
+// legendDesignElement.addEventListener("click", handleCollapsable);
+// legendRellenaElement.addEventListener("click", handleCollapsable);
+// legendComparteElement.addEventListener("click", handleCollapsable);
+
+
+const legendElements = document.querySelectorAll('.legend');
+// const collapsableElements = document.querySelectorAll('.collapsable');
+/* console.log(legendElements[0].parentElement); */
+
+function handleCollapsable(event) {
+  const collapsableElement = event.currentTarget.parentElement;
+  collapsableElement.classList.toggle('collapsable--on');
 }
-
-legendDesignElement.addEventListener("click", handleCollapsable);
-legendRellenaElement.addEventListener("click", handleCollapsable);
-legendComparteElement.addEventListener("click", handleCollapsable);
+for (const legendElement of legendElements) {
+  legendElement.addEventListener('click', handleCollapsable);
+}
