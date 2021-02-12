8ccd9b176c4ea54d9abb3cb2140a5fcbcb0f14b8
ctalloc <cecilia.tallo@gmail.com>
2021-02-12 12:30:19 +0100

    Disable share button on click

---
+++ src/js/05 resetBtn.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function handleResetBtn() {
+  resetUserData();
+  console.log("estoy funcionando");
+  resetLocalStorage();
+  getFromLocalStorage();
+  hiddenElement.classList.add("comparte__nav2--hidden");
+}
+function resetUserData() {
+  (userData.photo = ""),
+    (userData.palette = ""),
+    (userData.name = ""),
+    (userData.job = ""),
+    (userData.email = ""),
+    (userData.phone = ""),
+    (userData.linkedin = ""),
+    (userData.github = ""),
+    //updateAllInputs();
+    console.log("hola");
+}
+
+function resetLocalStorage() {
+  localStorage.removeItem("userData");
+}
+
+const resetBtnElement = document.querySelector(".js-btn-reset");
+resetBtnElement.addEventListener("click", handleResetBtn);
--- src/js/06 share.js
+++ src/js/06 share.js
@@ -9,13 +9,8 @@ function handleCreateBtn(ev) {
   ev.preventDefault();
   hiddenElement.classList.remove("comparte__nav2--hidden");
   console.log("Mis datos", getUserData());
-  const url =
-    "https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/";
+  const url = "https://profileawesome.herokuapp.com/card";
   const data = getUserData();
-  // de momento lo dejamos aquí para comprobar hasta que el servidor funciona => cuando funciona el servidor la consatnte tendra que tener el enlace que devuelve el servidor
-  const urlTweet = "https://www.wikipedia.org";
-  twitterBtnElement.href = `https://twitter.com/intent/tweet?text=Mirad%20mi%20nueva%20tarjeta%20${urlTweet}`;
-  
   fetch(url, {
     method: "POST",
     body: JSON.stringify(data),
@@ -29,37 +24,15 @@ function handleCreateBtn(ev) {
       if (data.success === true) {
         cardResultElement.innerHTML = data.cardURL;
         cardResultElement.href = data.cardURL;
-        // aqui el código final de twitter - adap[tar si queremos algún texto personalizado]
-        // twitterBtnElement.href = `https://twitter.com/intent/tweet?text=${data.cardURL}`;
+        twitterBtnElement.href = `https://twitter.com/intent/tweet?text=Mi%20nueva%20tarjeta%20${data.cardURL}`;
       } else {
         cardResultElement.innerHTML = data.error;
       }
     });
+  disableBtn()
 }
-/*
-function handleCreateBtn(ev) {
-  ev.preventDefault();
-  console.log('Mis datos', getUserData());
-
-  const url = 'https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/';
-  const data = getUserData();
-
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Server response:', data);
-      if (data.success === true) {
-        cardResultElement.innerHTML = data.cardURL;
-      } else {
-        cardResultElement.innerHTML = data.error;
-      }
-    });
-}
-*/
 createBtn.addEventListener("click", handleCreateBtn);
+
+function disableBtn() {
+  createBtn.disabled=true;
+}
